fd758efc7ea3d3aece08e3aeb7bf99ab116cb6e8
ilyau <ukhanov.ilya.67@gmail.com>
2023-03-09 15:50:10 +0100

    Initial commit

---
+++ deploy/api/index.php
@@ -0,0 +1,20 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Tuupola\Middleware\HttpBasicAuthentication;
+use \Firebase\JWT\JWT;
+require __DIR__ . '/../vendor/autoload.php';
+ 
+
+$app = AppFactory::create();
+
+
+$app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
+    $array = [];
+    $array ["nom"] = $args ['name'];
+    $response->getBody()->write(json_encode ($array));
+    return $response;
+});
+
+$app->run ();
