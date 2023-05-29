dc843b0034fe230779f929b118fbeab18e8e0eb1
ilyau <ukhanov.ilya.67@gmail.com>
2023-05-29 16:40:33 +0200

    Deploy - fix les settings du PDO

--- backend/settings.php
+++ backend/settings.php
@@ -4,10 +4,12 @@ define('APP_ROOT', __DIR__);
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Tools\DsnParser;
 
-$dsnParser = new DsnParser();
-$connectionParams = $dsnParser
-    ->parse('pdo-pgsql://root:FD69TAD1OrTY9ys7TePmQq6da38xk8gq@dpg-chq90iu7avjb90m6m960-a.frankfurt-postgres.render.com/nfe114_db');
-$conn = DriverManager::getConnection($connectionParams);
+//$dsnParser = new DsnParser();
+//$connectionParams = $dsnParser
+    // ->parse('pdo-pgsql://root:FD69TAD1OrTY9ys7TePmQq6da38xk8gq@dpg-chq90iu7avjb90m6m960-a.frankfurt-postgres.render.com/nfe114_db');
+//	->parse('pdo_pgsql://host=dpg-chq90iu7avjb90m6m960-a.frankfurt-postgres.render.com;port=5432;dbname=nfe114_db;User ID=root;Password=FD69TAD1OrTY9ys7TePmQq6da38xk8gq;');
+
+//$conn = DriverManager::getConnection($connectionParams);
 
 return [
     'settings' => [
@@ -33,16 +35,16 @@ return [
             // needs a 'path' parameter and doesn't use most of the ones shown in this example).
             // Refer to the Doctrine documentation to see the full list
             // of valid parameters: https://www.doctrine-project.org/projects/doctrine-dbal/en/current/reference/configuration.html
-            // 'connection' => [
-            //     'driver' => 'pdo_pgsql',
-            //     'host' => 'dpg-chq90iu7avjb90m6m960-a.frankfurt-postgres.render.com',
-            //     'port' => 5432,
-            //     'dbname' => 'nfe114_db',
-            //     'user' => 'root',
-            //     'password' => 'FD69TAD1OrTY9ys7TePmQq6da38xk8gq',
-            //     'charset' => 'UTF8'
-            // ]
-            'connection' => $conn
+             'connection' => [
+                 'driver' => 'pdo_pgsql',
+                 'host' => 'dpg-chq90iu7avjb90m6m960-a.frankfurt-postgres.render.com',
+                 'port' => 5432,
+                 'dbname' => 'nfe114_db',
+                 'user' => 'root',
+                 'password' => 'FD69TAD1OrTY9ys7TePmQq6da38xk8gq',
+                 'charset' => 'UTF8'
+             ]
+//            'connection' => $conn
         ]
     ]
 ];
